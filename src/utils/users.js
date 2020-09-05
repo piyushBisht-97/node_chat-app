13a9cb7bec3dfd1d1e6640b00038ee78969fdadc
Piyush <piyushbisht250@gmail.com>
2020-09-06 00:27:12 +0530

    Init commit

---
+++ src/utils/users.js
@@ -0,0 +1,45 @@
+const users = [];
+//add user ,removeuser,getuser
+const addUser = ({ id, username, room }) => {
+  //clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+  //validate the data
+  if (!username || !room) {
+    return {
+      error: "usernmae and room are required",
+    };
+  }
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+  if (existingUser) {
+    return {
+      eror: "username is in use",
+    };
+  }
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+const removeUser = (id) => {
+  const index = users.findIndex((user) => {
+    return user.id === id;
+  });
+  if (index != -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
